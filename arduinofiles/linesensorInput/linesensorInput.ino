7672130623430601e5e3b7b3ec24a823c41d1e10
Syed Farhan <farhan.tuba@gmail.com>
2019-02-19 17:20:28 +0530

    Made necessary changes.

--- arduinofiles/linesensorInput/linesensorInput.ino
+++ arduinofiles/linesensorInput/linesensorInput.ino
@@ -1,12 +1,67 @@
+
+#define BL 190
+#define BM 247
+#define BR 167
+#define OT 500
+
+int state=0;
+//state---0 stop 1-right 2-left
 int analogPin1 = 1;  
 int analogPin2 = 2;
 int analogPin3 = 3;
-
 int Lef = 0;
 int Mid = 0;
 int Rig = 0;
 int value = 0;
 
+
+int motorA1 = 3; // Pin  2 of L293
+int motorA2 = 5; // Pin  7 of L293
+int motorB1 = 6; // Pin 10 of L293
+int motorB2 = 9;
+
+
+int velrm = 250; // Speed Of Motors (0-255)
+int vellm = 250; //speed while turning as 50 isnt sufficient.
+int velright = 200;
+int velleft =200;
+
+void forward(){
+  analogWrite(motorA1, velrm);
+    analogWrite(motorA2, 0);
+    analogWrite(motorB1, vellm);
+    analogWrite(motorB2, 0);
+    Serial.println("Forward");
+}
+void right(){
+  analogWrite(motorA1, velright);
+    analogWrite(motorA2, 0);
+    analogWrite(motorB1, 0);
+    analogWrite(motorB2, velright);
+    state=1;
+    Serial.println("Right");
+}
+void left(){
+  analogWrite(motorA1, 0);
+    analogWrite(motorA2, velleft);
+    analogWrite(motorB1, velleft);
+    analogWrite(motorB2, 0);
+    state=2;
+    Serial.println("Left");
+}
+void back(){
+   analogWrite(motorA1, 0);
+    analogWrite(motorA2, velrm);
+    analogWrite(motorB1, 0);
+    analogWrite(motorB2, vellm);
+    Serial.println("Backward");
+}
+void stop_car(){
+  analogWrite(motorA1, 0);
+    analogWrite(motorA2, 0);
+    analogWrite(motorB1, 0);
+    analogWrite(motorB2, 0);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -14,9 +69,40 @@ void setup() {
   // put your setup code here, to run once:
 
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
   lineSenor();
+      
+
+   if(Lef<BL&&Mid>BM&&Rig<BR)
+   {
+    forward();
+   
+   }
+   if(Lef>BL&&Rig<BR)
+   {
+    right();
+   
+   }
+   if(Lef<BL&&Rig>BR)
+   {
+    left();
+   
+   }
+   if(Lef<BL&&Mid<BM&&Rig<BR)
+   {
+    if(state==1){
+      left();
+    }
+    else if(state==2){
+      right();
+    }
+    else{
+    stop_car();
+    }
+   }
+   
 }
 void lineSenor()
 {
