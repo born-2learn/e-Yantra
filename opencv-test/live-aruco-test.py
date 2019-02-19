b6950cc509b7141e1ac95baad209d6958c9ded5e
Syed Farhan <farhan.tuba@gmail.com>
2019-02-19 13:00:21 +0530

    Made necessary changes.

--- opencv-test/live-aruco-test.py
+++ opencv-test/live-aruco-test.py
@@ -1,6 +1,17 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import cv2
+import time
 import csv
 from . import aruco_lib
+
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+time.sleep(0.1)
 
 cap = cv2.VideoCapture(0)
 
@@ -9,6 +20,35 @@ id_s = []  # list to store SIM ID, Aruco ID pair
 
 if __name__ == '__main__':
     ids = []  # list to store Aruco IDs Scanned
+
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        image = frame.array
+
+        det_aruco_list = aruco_lib.detect_Aruco(image)
+        if det_aruco_list:
+            image = aruco_lib.mark_Aruco(frame, det_aruco_list)
+            robot_state = aruco_lib.calculate_Robot_State(image, det_aruco_list)
+            id_raw = robot_state.keys()
+            for i in id_raw:
+                if i not in ids:
+                    ids.append(i)
+                    ids.sort()
+        # show the frame
+        cv2.imshow("Frame", image)
+        key = cv2.waitKey(1) & 0xFF
+
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+            print(ids)
+            break
+
+    '''        
     while (True):
         ret, frame = cap.read()
         det_aruco_list = aruco_lib.detect_Aruco(frame)
@@ -24,3 +64,4 @@ if __name__ == '__main__':
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+    '''
