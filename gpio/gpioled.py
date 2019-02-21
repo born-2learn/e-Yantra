c5cca699c31988edeff5da6599e15bba577b61d9
Syed Farhan <farhan.tuba@gmail.com>
2019-02-21 19:44:21 +0530

    Made necessary changes.

--- gpio/gpioled.py
+++ gpio/gpioled.py
@@ -24,74 +24,17 @@ GPIO.setup(blue, GPIO.OUT)
 
 #choosing a frequency for pwm
 
-Freq = 100
-
-#defining the pins that are going to be used with PWM
-
-RED = GPIO.PWM(red, Freq)
-
-GREEN = GPIO.PWM(green, Freq)
-
-BLUE = GPIO.PWM(blue, Freq)
-
+print("Testing led out, Press CTRL+C to exit")
 try:
-    while RUNNING:
-        # lighting up the pins. 100 means giving 100% to the pin
+     print("set GIOP high")
+     GPIO.output(blue, GPIO.HIGH)
+     time.sleep(5)
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+   print("Keyboard interrupt")
 
-        RED.start(100)
+except:
+   print("some error")
 
-        GREEN.start(1)
-
-        BLUE.start(1)
-
-        # For anode RGB LED users, if you want to start with RED too the only thing to be done is defining RED as one and GREEN and BLUE as 100.
-
-        for x in range(1, 101):
-            GREEN.ChangeDutyCycle(x)
-
-            # for anode LED users, just change x with 101-x
-
-            # and for delay time.sleep is used. You can chance the duration of the colors with changing the time from here
-
-            time.sleep(0.05)
-
-
-
-
-
-        for x in range(1, 101):
-            RED.ChangeDutyCycle(101 - x)
-            time.sleep(0.025)
-
-
-
-
-        for x in range(1, 101):
-            GREEN.ChangeDutyCycle(101 - x)
-
-            BLUE.ChangeDutyCycle(x)
-
-            time.sleep(0.025)
-
-
-
-        for x in range(1, 101):
-            RED.ChangeDutyCycle(x)
-
-            time.sleep(0.025)
-
-
-
-
-
-
-
-
-
-except KeyboardInterrupt:
-    RUNNING = False
-
-    GPIO.cleanup()
-
-#the purpose of this part is, when you interrupt the code, it will stop the while loop and turn off the pins, which means your LED won't light anymore
-
+finally:
+   print("clean up")
+   GPIO.cleanup() # cleanup all GPIO
