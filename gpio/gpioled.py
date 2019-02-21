0e41ab75adfe1c3a43171b3024d9d5140023f6a2
Syed Farhan <farhan.tuba@gmail.com>
2019-02-21 19:47:07 +0530

    Made necessary changes.

--- gpio/gpioled.py
+++ gpio/gpioled.py
@@ -1,40 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-
-#closing the warnings when you are compiling the code GPIO.setwarnings(False)
-
-RUNNING = True
-
-#defining the pins
-
-green = 22
-
-red = 17
-
-blue = 27
-
-#defining the pins as output
-
-GPIO.setup(red, GPIO.OUT)
-
-GPIO.setup(green, GPIO.OUT)
-
-GPIO.setup(blue, GPIO.OUT)
-
-#choosing a frequency for pwm
-
-print("Testing led out, Press CTRL+C to exit")
-try:
-     print("set GIOP high")
-     GPIO.output(blue, GPIO.HIGH)
-     time.sleep(5)
-except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-   print("Keyboard interrupt")
-
-except:
-   print("some error")
-
-finally:
-   print("clean up")
-   GPIO.cleanup() # cleanup all GPIO
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+print "LED on"
+GPIO.output(18,GPIO.HIGH)
+time.sleep(5)
+print "LED off"
+GPIO.output(18,GPIO.LOW)
