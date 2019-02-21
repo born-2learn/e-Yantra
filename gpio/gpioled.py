eb9f88715a152277e612ece0974f8f1358fcef88
Syed Farhan <farhan.tuba@gmail.com>
2019-02-21 19:36:40 +0530

    Made necessary changes.

--- gpio/gpioled.py
+++ gpio/gpioled.py
@@ -1,176 +1,96 @@
-import sys, time
-# Here, we import two built-in libraries - functions, variables, etc. that have already been defined and that we can call on.
-
 import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
 
-# This is a Raspbian library that allows us to use Python to control GPIO pins.
+#closing the warnings when you are compiling the code GPIO.setwarnings(False)
 
-redPin = 11
-greenPin = 15
-bluePin = 13
+RUNNING = True
 
+#defining the pins
 
-# Now, we define some variables - which pins we'll be connecting to which RGB lead. This step isn't necessary, but it makes it easier to change pins if you need to.
+green = 15
 
-def blink(pin):
-    # Here, we define a function. A function is a set of steps that the computer will execute in the order that we give to it. This particular function will require one variable in order to run: the number of the pin that we want to light up.
+red = 11
 
-    GPIO.setmode(GPIO.BOARD)
-    # We start our function off by referencing one of our libraries, GPIO. Then, we tell it that we want to choose a naming convention for the GPIO pins (of which there are two), so that the program knows which ones we're referring to. Then, we tell it to use the "board" convention (rather than the BMC convention), again using the GPIO library.
+blue = 13
 
-    GPIO.setup(pin, GPIO.OUT)
-    # Now, once again using the GPIO library, we tell the Pi that we want to use a pin. But first, we need to specify which pin and how we want to use it - input or output. Here, "pin" is the variable that will be plugged into blink(pin).
+#defining the pins as output
 
-    GPIO.output(pin, GPIO.HIGH)
+GPIO.setup(red, GPIO.OUT)
 
+GPIO.setup(green, GPIO.OUT)
 
-# Now that the pin has been set up, this line turns it on. You should recognize the syntax by now; here we're using the same variable "pin" and using the GPIO library to set the pin to a value of "high". In the case of GPIO pins, a "high" value results in 3.3v being passed, or "on", whereas a "low" value results in 0v being passed, or "off".
+GPIO.setup(blue, GPIO.OUT)
 
-# Now we're going to define a function to turn lights off. It's going to look almost identical to the previous function, only we'll be setting the pins back to low.
+#choosing a frequency for pwm
 
-def turnOff(pin):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
+Freq = 100
 
+#defining the pins that are going to be used with PWM
 
-# We need to define some functions specific to color using the functions we just made. Recall that we're using pins 11, 13, and 15 for red, green, and blue. We'll start with functions that turn lights on. With this type of function, we won't require any parameters, so the brackets stay empty; calling this function later in the program will simply execute it.
+RED = GPIO.PWM(red, Freq)
 
-def redOn():
-    blink(redPin)
+GREEN = GPIO.PWM(green, Freq)
 
+BLUE = GPIO.PWM(blue, Freq)
 
-# Here, we give the "blink" function a pin number to use, 11. When we call redOn(), our blink function will begin executing with "pin" replaced by "redPin". Recall that, earlier, we set redPin equal to 11.
+try:
+    while RUNNING:
+        # lighting up the pins. 100 means giving 100% to the pin
 
-# Now we do the same thing for the rest of the colors.
+        RED.start(100)
 
-def greenOn():
-    blink(greenPin)
+        GREEN.start(1)
 
+        BLUE.start(1)
 
-def blueOn():
-    blink(bluePin)
+        # For anode RGB LED users, if you want to start with RED too the only thing to be done is defining RED as one and GREEN and BLUE as 100.
 
+        for x in range(1, 101):
 
-# Remember that when dealing with light, yellow is made by mixing red with green, cyan using green and blue, magenta with red and blue, and white by combining all three.
+        # for changing the width of PWM, this command is used
 
-def yellowOn():
-    blink(redPin)
-    blink(greenPin)
+        GREEN.ChangeDutyCycle(x)
 
+        # for anode LED users, just change x with 101-x
 
-def cyanOn():
-    blink(greenPin)
-    blink(bluePin)
+        # and for delay time.sleep is used. You can chance the duration of the colors with changing the time from here
 
+        time.sleep(0.05)
 
-def magentaOn():
-    blink(redPin)
-    blink(bluePin)
+        for x in range(1, 101):
+            RED.ChangeDutyCycle(101 - x)
+            time.sleep(0.025)
 
 
-def whiteOn():
-    blink(redPin)
-    blink(greenPin)
-    blink(bluePin)
 
 
-# Finally, we write the functions to turn lights off. Again, the syntax will be similar to the functions above.
+        for x in range(1, 101):
+            GREEN.ChangeDutyCycle(101 - x)
 
-def redOff():
-    turnOff(redPin)
+            BLUE.ChangeDutyCycle(x)
 
+            time.sleep(0.025)
 
-def greenOff():
-    turnOff(greenPin)
 
 
-def blueOff():
-    turnOff(bluePin)
+        for x in range(1, 101):
+            RED.ChangeDutyCycle(x)
 
+            time.sleep(0.025)
 
-def yellowOff():
-    turnOff(redPin)
-    turnOff(greenPin)
 
 
-def cyanOff():
-    turnOff(greenPin)
-    turnOff(bluePin)
 
 
-def magentaOff():
-    turnOff(redPin)
-    turnOff(bluePin)
 
 
-def whiteOff():
-    turnOff(redPin)
-    turnOff(greenPin)
-    turnOff(bluePin)
 
 
-# Now we're ready to define our main function; a loop that will constantly ask for user input and react by calling various functions. In this case, the user will be able to turn colors on and off. This program is written so that it will only accept a few pre-specified inputs. Otherwise, it will do nothing and display a message to that effect.
+except KeyboardInterrupt:
+    RUNNING = False
 
-# Again, we're not asking for any variables with this function, so the brackets stay empty. "while True" essentially says "while the program is running, do this", so it will run constantly until we quit.
+    GPIO.cleanup()
 
-def main():
-    while True:
-        cmd = input("Choose an option:")
-        # We define a new variable "cmd" to use later and set it equal to the user's input. It will also constantly prompt the user to choose an option.
+#the purpose of this part is, when you interrupt the code, it will stop the while loop and turn off the pins, which means your LED won't light anymore
 
-        # Now an "if" statement that will run only if the user input exactly matches the text within the quotation marks. Note that here we use two "equal to" symbols. In Python, and indeed most languages, a single "equal to" is used to change the value of a variable while doubles are used to compare whether or not variable values match each other.
-
-        if cmd == "red on":
-            redOn()
-        # "elif" stands for "else, if". This tacks onto the earlier "if" statement and adds new parameters, allowing us to create a chain of "else, if" statements easily.
-
-        elif cmd == "red off":
-            redOff()
-
-        elif cmd == "green on":
-            greenOn()
-
-        elif cmd == "green off":
-            greenOff()
-
-        elif cmd == "blue on":
-            blueOn()
-
-        elif cmd == "blue off":
-            blueOff()
-
-        elif cmd == "yellow on":
-            yellowOn()
-
-        elif cmd == "yellow off":
-            yellowOff()
-
-        elif cmd == "cyan on":
-            cyanOn()
-
-        elif cmd == "cyan off":
-            cyanOff()
-
-        elif cmd == "magenta on":
-            magentaOn()
-
-        elif cmd == "magenta off":
-            magentaOff()
-
-        elif cmd == "white on":
-            whiteOn()
-
-        elif cmd == "white off":
-            whiteOff()
-
-        else:
-            print("Not a valid command.")
-
-    return
-
-
-# With our last "else" statement, if the user attempts to input anything other than one of the commands specified above, it will tell the user that it is an invalid command. The last thing the function needs is that "return" so the program knows when to loop.
-
-main()
-# This is the last line of our code. When the program is run, this line will call the main function that we just defined and the loop will be set.
