ec7e9302489e66a1d3b10f2b0619b001e3b7100c
Syed Farhan <farhan.tuba@gmail.com>
2019-02-21 19:47:51 +0530

    Made necessary changes.

--- gpio/gpioled.py
+++ gpio/gpioled.py
@@ -3,8 +3,8 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-print "LED on"
+print("LED on")
 GPIO.output(18,GPIO.HIGH)
 time.sleep(5)
-print "LED off"
+print("LED off")
 GPIO.output(18,GPIO.LOW)
