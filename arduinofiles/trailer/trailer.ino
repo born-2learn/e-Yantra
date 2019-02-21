dddb9501747a1fbdb4038dc1f2ea9bce59e54f93
Syed Farhan <farhan.tuba@gmail.com>
2019-02-21 13:48:45 +0530

    Made necessary changes.

---
+++ arduinofiles/trailer/trailer.ino
@@ -0,0 +1,135 @@
+
+#define LT 80
+#define RT 80
+#define MT 80
+#define OT 500
+
+int state=0;
+//state---0 stop 1-right 2-left
+int analogPin1 = 1;  
+int analogPin2 = 2;
+int analogPin3 = 3;
+int L = 0;
+int M = 0;
+int R = 0;
+int value = 0;
+
+
+int motorA1 = 3; // Pin  2 of L293
+int motorA2 = 5; // Pin  7 of L293
+int motorB1 = 6; // Pin 10 of L293
+int motorB2 = 9;
+
+
+int vellm = 160; // Speed Of Motors (0-255)
+int velrm = 160; //speed while turning as 50 isnt sufficient.
+int velleft = 90;
+int velright =90;
+
+void forward(){
+  analogWrite(motorA1, velrm);
+    analogWrite(motorA2, 0);
+    analogWrite(motorB1, vellm);
+    analogWrite(motorB2, 0);
+    Serial.println("Forward");
+}
+void right(){
+  analogWrite(motorA1, velleft);
+    analogWrite(motorA2, 0);
+    analogWrite(motorB1, 0);
+    analogWrite(motorB2, velright);
+    state=1;
+    Serial.println("Right");
+}
+void left(){
+  analogWrite(motorA1, 0);
+    analogWrite(motorA2, velleft);
+    analogWrite(motorB1, velright);
+    analogWrite(motorB2, 0);
+    state=2;
+    Serial.println("Left");
+}
+void back(){
+   analogWrite(motorA1, 0);
+    analogWrite(motorA2, vellm);
+    analogWrite(motorB1, 0);
+    analogWrite(motorB2, velrm);
+    Serial.println("Backward");
+}
+void stop_car(){
+  analogWrite(motorA1, 0);
+    analogWrite(motorA2, 0);
+    analogWrite(motorB1, 0);
+    analogWrite(motorB2, 0);
+}
+
+void setup() {
+  Serial.begin(9600);
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(9,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  lineSenor();
+      
+
+   if(L<LT&&M>MT&&R<RT)
+   {
+    forward();
+    state=0;
+   
+   }
+   if(L<LT&&R>RT&&M<MT)
+   {
+    right();
+   
+   }
+   if(L>LT&&R<RT&&M<MT)
+   {
+    left();
+    state=0;
+   
+   }
+   if(L<LT&&M<MT&&R<RT)
+   {
+    if(state==1){
+      left();
+      state=0;
+    }
+    if(state==2){
+      right();
+      state=0;
+    }
+    
+   }
+   if((L>LT&&R>RT)||(R>RT&&M>MT)||(L>LT&&M>MT)){
+    stop_car();
+   }
+   
+}
+void lineSenor()
+{
+ 
+  //Serial.print("you you you");
+  //digitalWrite(13, HIGH);
+  //Serial.println("me me me");
+  R = analogRead(analogPin1); // read the input pin
+  M = analogRead(analogPin2);     // read the input pin
+  L = analogRead(analogPin3);  // read the input pin
+ // value = (value1+value2+value3)/3;
+ // Serial.println(value);
+ 
+ Serial.print("Left: ");
+  Serial.print(L);
+   Serial.print(" Center: ");
+    Serial.print(M);
+     Serial.print(" Right: ");
+      Serial.print(R);
+      Serial.println();
+}
