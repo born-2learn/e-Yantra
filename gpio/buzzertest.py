7ae8901a25e8165e3f1b2d54cbb209a1e744a68b
Syed Farhan <farhan.tuba@gmail.com>
2019-02-22 07:29:23 +0530

    Made necessary changes.

--- gpio/buzzertest.py
+++ gpio/buzzertest.py
@@ -2,18 +2,21 @@ from gpiozero import Buzzer
 from gpiozero import LED
 from time import sleep
 
-red = LED(17)
-blue = LED(27)
-green = LED(4)
+green = LED(17)
+red = LED(27)
+blue = LED(18)
 buzzer = Buzzer(3)
 
 def ledOn():
     red.on()
-    sleep(5)
+    sleep(2)
     red.off()
     blue.on()
-    sleep(5)
+    sleep(2)
     blue.off()
+    green.on()
+    sleep(2)
+    green.off()
 
 def ringBuzzer():
     buzzer.on()
