4229c396a8b28fab4257c75654ee38aaec0ae453
Syed Farhan <farhan.tuba@gmail.com>
2019-02-22 12:37:29 +0530

    Made necessary changes.

--- serialcode/servotest.py
+++ serialcode/servotest.py
@@ -9,10 +9,10 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
+    k=float(input('duty cycle'))
+    p.ChangeDutyCycle(k)
+
+
 
 except KeyboardInterrupt:
   p.stop()
