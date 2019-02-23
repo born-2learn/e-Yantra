4b378983a856478799c5a5f9310366926fe7c3d6
Syed Farhan <farhan.tuba@gmail.com>
2019-02-23 23:10:25 +0530

    Made necessary changes.

--- serialcode/serialcode.py
+++ serialcode/serialcode.py
@@ -5,7 +5,7 @@ while 1:
     if(ser.in_waiting >0):
         input = ser.read()
 
-        input_number = ord(input)
+        input_number = input
 
-        print("Read input back: " + str(input_number))
+        print(input_number)
         #for i in range(0, 3):
