269381887e60b28e0b94fd696681f914ec499d72
Syed Farhan <farhan.tuba@gmail.com>
2019-02-23 22:55:19 +0530

    Made necessary changes.

--- serialcode/serialcode.py
+++ serialcode/serialcode.py
@@ -4,7 +4,7 @@ ser = Serial('/dev/ttyUSB0', 9600)
 while 1:
     if(ser.in_waiting >0):
         line = ser.readline()
-
+        print('Accepted val')
         print(line)
         if(line=='1'):
             print('true')
