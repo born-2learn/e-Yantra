a6ec1195336c20f49979846c05d1c6482575e417
Syed Farhan <farhan.tuba@gmail.com>
2019-02-23 22:46:53 +0530

    Made necessary changes.

--- serialcode/serialcode.py
+++ serialcode/serialcode.py
@@ -3,8 +3,8 @@ from serial import Serial
 ser = Serial('/dev/ttyUSB0', 9600)
 while 1:
     if(ser.in_waiting >0):
-        line = ser.readline()
-        line=line[0:-4]
+        l = ser.readline()
+        line=ord(l)
         print(line)
         if(line=='1'):
             print('true')
