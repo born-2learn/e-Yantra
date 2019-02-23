3b9ac0fbd8e3153ad831f5dfcca0ecb117d3792e
Syed Farhan <farhan.tuba@gmail.com>
2019-02-23 23:16:17 +0530

    Made necessary changes.

--- serialcode/serialcode.py
+++ serialcode/serialcode.py
@@ -5,7 +5,8 @@ while 1:
     if(ser.in_waiting >0):
         input = ser.read()
 
-        input_number = chr(input)
+        input_number = ord(input)
 
-        print(input_number)
+        if(input_number==49):
+            print('true')
         #for i in range(0, 3):
