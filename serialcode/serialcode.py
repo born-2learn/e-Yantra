64f1e04740d95952dd59a3d18949a3bd4ffcb47c
Syed Farhan <farhan.tuba@gmail.com>
2019-02-23 23:03:42 +0530

    Made necessary changes.

--- serialcode/serialcode.py
+++ serialcode/serialcode.py
@@ -3,8 +3,9 @@ from serial import Serial
 ser = Serial('/dev/ttyUSB0', 9600)
 while 1:
     if(ser.in_waiting >0):
-        line = ser.readline()
-        print('Accepted val')
-        print(line)
-        if(line=='1'):
-            print('true')
+        for i in range(0, 3):
+            input = ser.read()
+
+            input_number = ord(input)
+
+            print("Read input back: " + str(input_number))
