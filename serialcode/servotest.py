f3d3560db65f37f3cabad62c9d63aeadae8f105e
Syed Farhan <farhan.tuba@gmail.com>
2019-02-24 12:01:16 +0530

    Made necessary changes.

--- serialcode/servotest.py
+++ serialcode/servotest.py
@@ -1,19 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 17
+arm = 13
+grab = 19
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(arm, GPIO.OUT)
+GPIO.setup(grab, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+s1 = GPIO.PWM(arm, 50) # GPIO 17 for PWM with 50Hz
+s2 = GPIO.PWM(grab, 50) # GPIO 17 for PWM with 50Hz
+
+s1.start(2.5) # Initialization
+s2.start(2.5)
 try:
   while True:
     k=float(input('duty cycle'))
-    p.ChangeDutyCycle(k)
+    s1.ChangeDutyCycle(k)
+    s2.ChangeDutyCycle(k)
 
 
 
 except KeyboardInterrupt:
-  p.stop()
+  s1.stop()
+  s2.stop()
   GPIO.cleanup()
